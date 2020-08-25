1fd20192bd20247774339b7b6e4944974d8ec9f8
DuzeyKadir\KadirOcsoy <>
2020-08-25 11:16:42 +0300

    Add project files.

---
+++ core1/Models/SilAPI.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace core1.Models
+{
+    public class SilAPI:DbContext
+    {
+        public SilAPI(DbContextOptions<SilAPI> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<SilAPI> Courses { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<SilAPI>().ToTable("Course");
+        }
+    }
+}
---
+++ core1/Models/prof.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace core1.Models
+{
+    public class prof
+    {
+        public int id { get; set; }
+        public string ad { get; set; }
+        public string soyad { get; set; }
+    }
+}
